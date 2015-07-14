7e158d84d84b3a855e28133a612acaff75005f93
darkc0de <darkcode@e-m-a-i-l.me>
2015-07-14 16:30:02 -0400

    Cleaned up Limited's commit

--- client/views/js/DoritoMenu.js
+++ client/views/js/DoritoMenu.js
@@ -1,3 +1,12 @@
-if (Meteor.isClient) {
+Template.Splash.events = {
+    'click li': function(e) {
+        var overlay = $(e.currentTarget).attr("data-menu");
+        $('li').removeClass("active");
+        $($(e.currentTarget)).toggleClass("active");
+        $(".overlay[data-id=" + overlay + "]").toggleClass("active");
+    }
+}
 
+Template.Home.rendered = function() {
+    $('body').delay(10).addClass('load');
 }
