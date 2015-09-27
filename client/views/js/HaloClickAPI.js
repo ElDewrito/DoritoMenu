0722f7c92154ea436569545f29470bbadb32bd58
darkc0de <darkcode@e-m-a-i-l.me>
2015-09-27 05:38:40 -0400

    Log people out if their halo.click session dies.

--- client/views/js/HaloClickAPI.js
+++ client/views/js/HaloClickAPI.js
@@ -3,11 +3,13 @@ DoHaloClickMagic = function() {
         if (ServerCookies.ready()) {
             Meteor.call('HaloClickLogin', function(error, user) {
                 if (error) {
+                	Meteor.logout();
                     DisplayNotification(error.reason);
                     return;
                 }
                 return Meteor.loginWithPassword(user.email, user.password, function(error) {
                     if (error) {
+                    	Meteor.logout();
                         DisplayNotification(error.reason);
                     } else {
                         DisplayNotification("Logged into Halo.Click as " + Meteor.user().username);
